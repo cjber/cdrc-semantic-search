c101bcd37e57b562355c635245d2b0e454afb0ff
cjber <cjberragan@gmail.com>
2023-12-05 13:36:11 +0000

    refactor utility function

--- src/extract.py
+++ src/extract.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import docx
@@ -5,7 +6,7 @@ import pandas as pd
 import pypdf
 import tabula
 
-import src.params as params
+from src.common.utils import Paths, _add_metadata_to_document
 
 
 def _extract_tables_from_docx(table: docx.table.Table) -> pd.DataFrame:
@@ -29,8 +30,8 @@ def _extract_tables_from_pdf(file_path: Path) -> list[str]:
 
 def extract_text_from_document(file_path: Path) -> str:
     if file_path.suffix == ".pdf":
-        pdf = pypdf.PdfReader(open(file_path, "rb"))
-        text = "\n\n".join([page.extract_text() for page in pdf.pages])
+        doc = pypdf.PdfReader(open(file_path, "rb"))
+        text = "\n\n".join([page.extract_text() for page in doc.pages])
         tables = "\n\n".join(_extract_tables_from_pdf(file_path))
 
     elif file_path.suffix.startswith(".doc"):
@@ -38,14 +39,36 @@ def extract_text_from_document(file_path: Path) -> str:
         text = "\n\n".join([para.text for para in doc.paragraphs])
         tables = "\n\n".join([_extract_tables_from_docx(table) for table in doc.tables])
 
-    return f"Document Text: \n\n {text}\n\n Tables: \n\n {tables}"
+    metadata = _add_metadata_to_document(file_path.stem)
+
+    return (
+        f"Dataset Title: {metadata['title']}\n\n"
+        f"Dataset Notes: \n\n {metadata['notes']}\n\n"
+        f"Document Text: \n\n {text}\n\n"
+        f"Tables: \n\n {tables}"
+    )
+
+
+def add_metadata_to_document(doc_id):
+    with open(Paths.DATA_DIR / "catalogue-metadata.json") as f:
+        cmeta = json.load(f)
+    with open(Paths.DATA_DIR / "files-metadata.json") as f:
+        fmeta = json.load(f)
+
+    for fm in fmeta:
+        if doc_id == fm["id"]:
+            parent_id = fm["parent_id"]
+
+    for cm in cmeta:
+        if parent_id == cm["id"]:
+            return {"title": cm["title"], "notes": cm["notes"]}
 
 
 def main():
-    params.TXT_DIR.mkdir(exist_ok=True, parents=True)
+    Paths.NOTES_DIR.mkdir(exist_ok=True, parents=True)
 
-    for file in params.PDF_DIR.iterdir():
-        out_file = params.TXT_DIR / file.with_suffix(".txt").name
+    for file in Paths.PDF_DIR.iterdir():
+        out_file = Paths.NOTES_DIR / file.with_suffix(".txt").name
         if out_file.exists():
             continue
         text = extract_text_from_document(file)

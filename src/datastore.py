f5be059236ad4a58ea2791f6223a8a4ddde1d42d
cjber <cjberragan@gmail.com>
2023-12-06 16:50:11 +0000

    only update datastore with new or removed embeddings

--- src/datastore.py
+++ src/datastore.py
@@ -1,4 +1,6 @@
+import json
 import os
+from argparse import ArgumentParser
 
 from haystack import Pipeline
 from haystack.document_stores import PineconeDocumentStore
@@ -11,16 +13,25 @@ from haystack.nodes import (
     TextConverter,
 )
 
-from src.common.utils import Paths, _add_metadata_to_document
+from src.common.utils import Paths, _add_metadata_to_document, validate_files
+
+parser = ArgumentParser()
+parser.add_argument(
+    "--overwrite",
+    action="store_true",
+    help="Overwrite the existing index",
+)
+args = parser.parse_args()
 
 
-def create_docs(files):
+def create_docs(files, recreate_index: bool = False):
     document_store = PineconeDocumentStore(
         api_key=os.environ.get("PINECONE_API_KEY"),
         environment="gcp-starter",
         similarity="dot_product",
         embedding_dim=768,
         index="cdrc",
+        recreate_index=recreate_index,
     )
     preprocessor = PreProcessor(
         clean_empty_lines=True,
@@ -73,14 +84,45 @@ def create_docs(files):
         inputs=["Retriever"],
     )
 
+    _remove_old_docs(document_store)
+    missing = _missing_docs(document_store)
+
     for file in files:
+        filename = file.stem[:6] if file.stem.startswith("notes-") else file.stem
+        if filename not in missing and not recreate_index:
+            continue
         metadata = _add_metadata_to_document(file.stem)
         indexing_pipeline.run(file_paths=file, meta=metadata)
 
 
+def _remove_old_docs(document_store):
+    docs_remove, notes_remove, docs_missing, notes_missing = validate_files()
+
+    if docs_remove:
+        document_store.delete_documents(filters={"id": {"$in": docs_remove}})
+    if notes_remove:
+        document_store.delete_documents(filters={"id": {"$in": notes_remove}})
+
+
+def _missing_docs(document_store):
+    with open(Paths.DATA_DIR / "files-metadata.json", "r") as f:
+        file_data = json.load(f)
+    with open(Paths.DATA_DIR / "catalogue-metadata.json", "r") as f:
+        catalogue_data = json.load(f)
+
+    file_ids = {file["id"] for file in file_data}
+    catalogue_ids = {catalogue["id"] for catalogue in catalogue_data}
+    docs = {doc.to_dict()["meta"]["id"] for doc in document_store.get_all_documents()}
+
+    return docs.difference(file_ids | catalogue_ids)
+
+
 def main():
-    files = list(Paths.DOCS_DIR.iterdir())
-    create_docs(files)
+    docs = list(Paths.DOCS_DIR.iterdir())
+    notes = list(Paths.NOTES_DIR.iterdir())
+
+    create_docs(docs, recreate_index=args.overwrite)
+    create_docs(notes, recreate_index=False)  # never overwrite second run
 
 
 if __name__ == "__main__":

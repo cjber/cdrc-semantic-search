549543b9af7b3735f9549a97704733341295abea
cjber <cjberragan@gmail.com>
2023-12-16 12:15:10 +0000

    add prompt and urls

--- src/common/utils.py
+++ src/common/utils.py
@@ -5,7 +5,7 @@ from more_itertools import consume
 
 
 class Paths:
-    DATA_DIR = Path("./data")
+    DATA_DIR = Path("data")
     PROFILES_DIR = DATA_DIR / "profiles"
     DOCS_DIR = PROFILES_DIR / "docs"
     NOTES_DIR = PROFILES_DIR / "notes"
@@ -16,6 +16,24 @@ class Consts:
     INDEX_NAME = "cdrc-index"
     HF_EMBED_MODEL = None
     EMBED_DIM = 384
+    PROMPT = (
+        "Below is the data profile for a dataset from the CDRC Data Catalogue.\n"
+        "1. Summarise the data profile in under 50 words.\n"
+        "2. Explain the relevance of the dataset to the following query, using your own knowledge or the documents.\n\n"
+        "For each unique dataset, structure your answer as follows:\n\n"
+        "Title: <dataset title>\n\n"
+        "Summary: <dataset summary>\n\n"
+        "Relevance: <dataset relevance to 'Query'>\n\n"
+        "\n---------------------\n"
+        "Query: {query_str}\n\n"
+        "Data profile:\n"
+        "\n---------------------\n{context_str}\n---------------------\n\n"
+    )
+
+
+class Urls:
+    API_URL = "https://data.cdrc.ac.uk/api/3/action/current_package_list_with_resources"
+    LOGIN_URL = "https://data.cdrc.ac.uk/user/login"
 
 
 def _add_metadata_to_document(doc_id: str) -> dict:
@@ -36,37 +54,6 @@ def _add_metadata_to_document(doc_id: str) -> dict:
         if main_id == catalogue_meta["id"]:
             return {
                 "title": catalogue_meta["title"],
-                # "notes": catalogue_meta["notes"],
                 "id": catalogue_meta["id"],
                 "url": catalogue_meta["url"],
             }
-
-
-def validate_files(
-    remove_old_files: bool = True,
-) -> tuple[set[str], set[str], set[str], set[str]]:
-    with open(Paths.DATA_DIR / "files-metadata.json", "r") as f:
-        file_data = json.load(f)
-    with open(Paths.DATA_DIR / "catalogue-metadata.json", "r") as f:
-        catalogue_data = json.load(f)
-
-    file_ids = {file["id"] for file in file_data}
-    catalogue_ids = {catalogue["id"] for catalogue in catalogue_data}
-
-    docs = list(Paths.DOCS_DIR.iterdir())
-    notes = list(Paths.NOTES_DIR.iterdir())
-
-    doc_ids = {doc.stem for doc in docs}
-    note_ids = {note.stem[6:] for note in notes}
-
-    docs_remove = doc_ids.difference(file_ids)
-    notes_remove = note_ids.difference(catalogue_ids)
-
-    if remove_old_files:
-        consume(f.unlink() for f in docs if f.stem in docs_remove)
-        consume(f.unlink() for f in notes if f.stem[6:] in notes_remove)
-
-    docs_missing = file_ids.difference(doc_ids)
-    notes_missing = catalogue_ids.difference(note_ids)
-
-    return docs_remove, notes_remove, docs_missing, notes_missing

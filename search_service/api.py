8621be64c63488970ecc9731ef89feb4095f597b
cjber <cjberragan@gmail.com>
2024-01-25 09:24:04 +0000

    update output format to nested json

--- search_service/api.py
+++ search_service/api.py
@@ -25,26 +25,28 @@ def index():
 
 
 @app.post("/query")
-async def query(q: str) -> list[dict]:
+async def query(q: str) -> dict:
     model.run(q)
-    return [{"results_id": uuid4()}] + model.processed_response
+    return {"results_id": uuid4(), "results_content": model.processed_response}
 
 
 @app.get("/explain/{results_id}")
 async def explain(response_num: int, results_id: UUID) -> dict:
     model.explain_dataset(response_num)
     return {
-        "results_id": results_id,
         "explained_response": model.explained_response,
-        "related_dataset": model.processed_response[response_num],
+        "metadata": {
+            "results_id": results_id,
+            "related_dataset": model.processed_response[response_num],
+        },
     }
 
 
 @app.get("/results/{results_id}")
-async def results(results_id: UUID) -> list[dict]:
-    return [{"results_id": results_id}] + model.processed_response
+async def results(results_id: UUID) -> dict:
+    return {"results_id": results_id, "results_content": model.processed_response}
 
 
 @app.post("/query_str/{results_id}")
 async def query_str(results_id: UUID) -> dict:
-    return {"results_id": results_id, "query": model.query}
+    return {"query": model.query, "metadata": {"results_id": results_id}}

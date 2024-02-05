f874e7360a3324a085110186f0fa8ed478f12b3b
cjber <cjberragan@gmail.com>
2024-02-05 17:24:25 +0000

    ensure fastapi routes unique model for each uuid

--- search_service/api.py
+++ search_service/api.py
@@ -1,22 +1,20 @@
-from fastapi import FastAPI
-
-from src.common.utils import Settings
-from pydantic import BaseModel
-from src.model import LlamaIndexModel
 from uuid import UUID, uuid4
 
+from fastapi import Depends, FastAPI
 
-class ResponsePost(BaseModel):
-    results_id: UUID = uuid4()
-
-
-def create_app():
-    app = FastAPI()
-    model = LlamaIndexModel(**Settings().model.model_dump())
-    return app, model
+from src.common.utils import Settings
+from src.model import LlamaIndexModel
 
 
-app, model = create_app()
+app = FastAPI()
+model_instances = {}
+query_mapping = {}
+
+
+def get_model(results_id: UUID) -> LlamaIndexModel:
+    if results_id not in model_instances:
+        model_instances[results_id] = LlamaIndexModel(**Settings().model.model_dump())
+    return model_instances[results_id]
 
 
 @app.get("/")
@@ -26,27 +24,42 @@ def index():
 
 @app.post("/query")
 async def query(q: str) -> dict:
+    results_id = uuid4()
+    query_mapping[results_id] = q
+    return {"results_id": results_id, "query": q}
+
+
+@app.get("/results/{results_id}")
+async def results(
+    results_id: UUID, model: LlamaIndexModel = Depends(get_model)
+) -> dict:
+    if results_id not in query_mapping:
+        return {"error": "No query found for the provided results_id"}
+
+    q = query_mapping[results_id]
     model.run(q)
-    return {"results_id": uuid4(), "results_content": model.processed_response}
+    return {
+        "results_content": model.processed_response,
+        "metadata": {
+            "results_id": results_id,
+            "query": q,
+        },
+    }
 
 
 @app.get("/explain/{results_id}")
-async def explain(response_num: int, results_id: UUID) -> dict:
+async def explain(
+    response_num: int, results_id: UUID, model: LlamaIndexModel = Depends(get_model)
+) -> dict:
+    if results_id not in query_mapping:
+        return {"error": "No query found for the provided results_id"}
+
     model.explain_dataset(response_num)
     return {
         "explained_response": model.explained_response,
         "metadata": {
             "results_id": results_id,
+            "query": query_mapping[results_id],
             "related_dataset": model.processed_response[response_num],
         },
     }
-
-
-@app.get("/results/{results_id}")
-async def results(results_id: UUID) -> dict:
-    return {"results_id": results_id, "results_content": model.processed_response}
-
-
-@app.post("/query_str/{results_id}")
-async def query_str(results_id: UUID) -> dict:
-    return {"query": model.query, "metadata": {"results_id": results_id}}
